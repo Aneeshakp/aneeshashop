999f70ce89dc53de27e8b438c0f46ffc1e16763d
unknown <aneeshasreeraj@gmail.com>
2025-07-17 22:03:44 +0530

    updated cartpage

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -8,8 +8,7 @@ export default function CartPage() {
           <img
             src="https://m.media-amazon.com/images/G/31/cart/empty/kettle-desaturated._CB424694257_.svg"
             alt="Empty Cart"
-            className="img-fluid"
-          />
+            className="img-fluid"/>
         </div>
 
         <div className="col-md-8 text-center text-md-start">
@@ -17,8 +16,7 @@ export default function CartPage() {
           <p className="lead mb-4">Shop today’s deals</p>
           <button
             className="btn"
-            style={{ backgroundColor: '#f7ca00', color: '#111', fontWeight: 'bold' }}
-          >
+            style={{ backgroundColor: '#f7ca00', color: '#111', fontWeight: 'bold' }}>
             Sign in to your account
           </button>
         </div>
